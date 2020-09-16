a8a6067dce851163d23fe36516e69e0c1829659c
Gabriel Milhomem <gabriell.mil@gmail.com>
2020-09-16 03:19:14 -0300

    LayoutMobile - Tela Lateral

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,25 +1,64 @@
-var listaMensagem = ["entra na sala..."];
-var listaPai = document.querySelector("#listaMensagem");
+var mensagens = [];
+var participantes = ["Joao", "Maria", "Fernando"];
+var fundoLateral = document.querySelector(".fundoLateral");
+
+enviarParticipante();
 
 function enviarMensagem() {
-    var input = document.querySelector(".caixaTexto");
-    listaMensagem.push(input.value);
-    listaPai.innerHTML = "";
-    input.value = "";
-    for(var i = 0; i < listaMensagem.length; i++) {
-        renderizarMensagem(listaMensagem[i]);
+    var listaPaiMensagens = document.querySelector("#listaMensagem");
+    var entrada = document.querySelector(".caixaTexto");
+    mensagens.push(entrada.value);
+    listaPaiMensagens.innerHTML = "";
+    entrada.value = "";
+    for(var i = 0; i < mensagens.length; i++) {
+        renderizarMensagem(mensagens[i], listaPaiMensagens);
     }
 }
 
-function renderizarMensagem(mensagem) {
+function renderizarMensagem(mensagem, ul) {
     var liNovo = document.createElement("li");
     liNovo.classList.add("mensagem");
     liNovo.innerHTML = " <p> <time> (09:22:38) </time> <span> <strong> João </strong> para <strong> Maria: </strong> </span> " + mensagem + "</p>";
-    listaPai.appendChild(liNovo);
+    ul.appendChild(liNovo);
     scrollarParaBaixo();
+}
+
+function enviarParticipante() {
+    var listaPaiParticipantes = document.querySelector("#listaParticipante");
+    listaPaiParticipantes.innerHTML = "";
+    for(var i = 0; i < participantes.length; i++) {
+        renderizarParticipante(participantes[i], listaPaiParticipantes);
+    }
+}
+
+function renderizarParticipante(nome, ul) {
+    var liNovo = document.createElement("li");
+    liNovo.classList.add("participante");
+    liNovo.innerHTML = " <div> <ion-icon name= 'person-circle'> </ion-icon> <span> " + nome + " </span> </div> <ion-icon class= 'verificado' name= 'checkmark-sharp'> </ion-icon>";
+    ul.appendChild(liNovo);
 }
 
 function scrollarParaBaixo() {
     var chatMensagem = document.querySelector("main");
-    chatMensagem.scrollTop = chatMensagem.scrollHeight;
+    chatMensagem.scrollTop = chatMensagem.scrollHeight - chatMensagem.clientHeight;
+}
+
+function temBarraLateral(existeBarra) {
+    var menuLateral = document.querySelector("aside");
+    if(!existeBarra) {
+        fundoLateral.style.display = "block";
+        setTimeout(esperarFundoLateral, 300);
+        menuLateral.classList.toggle("asideNaTela");
+    }
+
+    else {
+        fundoLateral.classList.toggle("fundoPretoLateral");
+        menuLateral.classList.toggle("asideNaTela");
+        fundoLateral.style.display = "none";
+    }
+
+}
+
+function esperarFundoLateral() {
+    fundoLateral.classList.toggle("fundoPretoLateral");
 }
